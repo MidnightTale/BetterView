c647f5323cd5eeb7aa038f016111c10f3e4955af
booky10 <boooky10@gmail.com>
2025-07-14 16:29:09 +0200

    Implement support for fabric 1.21.6/1.21.7

---
+++ fabric-1217/build.gradle.kts
@@ -0,0 +1,46 @@
+import net.fabricmc.loom.task.AbstractRemapJarTask
+
+plugins {
+    alias(libs.plugins.loom)
+}
+
+repositories {
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") {
+        mavenContent { snapshotsOnly() }
+    }
+}
+
+dependencies {
+    minecraft(libs.minecraft.v1217)
+    mappings(loom.layered {
+        officialMojangMappings()
+        parchment(variantOf(libs.parchment.v1217) { artifactType("zip") })
+    })
+    modImplementation(libs.fabric.loader)
+
+    // common project setup
+    api(projects.common)
+
+    // adventure platform for better integration with everything
+    modImplementation(libs.adventure.platform.fabric.v1217)
+    include(libs.adventure.platform.fabric.v1217)
+
+    // depend on moonrise for chunk loading stuff
+    modApi(libs.moonrise.v1217)
+}
+
+tasks.named<ProcessResources>("processResources") {
+    inputs.property("version", project.version)
+    filesMatching("fabric.mod.json") {
+        expand("version" to project.version)
+    }
+}
+
+tasks.withType<AbstractRemapJarTask> {
+    archiveBaseName = "${rootProject.name}-${project.name}".lowercase()
+}
+
+loom {
+    accessWidenerPath = file("src/main/resources/betterview.accesswidener")
+    mixin.defaultRefmapName = "${rootProject.name}-${project.name}-refmap.json".lowercase()
+}
